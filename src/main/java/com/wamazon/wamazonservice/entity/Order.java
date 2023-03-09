764d0477318473160f2d34ccc47b321e7dd0df5f
Тимур Соколов <sokolov.tima@gmail.com>
2023-03-09 17:01:47 +0400

    Добавил order

--- src/main/java/com/wamazon/wamazonservice/entity/Order.java
+++ src/main/java/com/wamazon/wamazonservice/entity/Order.java
@@ -9,16 +9,16 @@ import lombok.Setter;
 
 import java.util.List;
 
-@Entity(name = "order")
+@Entity(name = "\"order\"")
 @Getter
 @Setter
 public class Order extends IdentifiableEntity {
 
     @ManyToMany
     @JoinTable(
-            name = "order_id",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "order_product")
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
     )
     private List<Product> products;
 }
